34c7de13fad2e66f2abd77749f692f3d1e46b7b7
CarlosJtdp <carlostomalaupse@gmail.com>
2024-12-18 08:28:49 -0500

    Update .config

--- src/config.js
+++ src/config.js
@@ -1,9 +1,9 @@
 import {config}from 'dotenv'
 config()
 
-export const BD_HOST=process.env.BD_HOST || "localhost"
-export const BD_DATABASE=process.env.BD_DATABASE|| "examenrifa"
-export const DB_USER=process.env.DB_USER|| "root"
-export const DB_PASSWORD=process.env.DB_PASSWORD||""
+export const BD_HOST=process.env.BD_HOST || "b6dh0pblovgmnahwnfkb-mysql.services.clever-cloud.com"
+export const BD_DATABASE=process.env.BD_DATABASE|| "b6dh0pblovgmnahwnfkb"
+export const DB_USER=process.env.DB_USER|| "uvtxldpzkanrir1c"
+export const DB_PASSWORD=process.env.DB_PASSWORD||"EDTBeNrD20ea3rvhxeJB"
 export const DB_PORT=process.env.DB_PORT|| 3306
 export const PORT= process.env.PORT|| 3000
